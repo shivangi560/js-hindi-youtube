b11dc5d5a9083c31f6a3131224afbd6dbbe4ce52
shivangi prajapati <kumarisivangi754@gmail.com>
2026-02-03 18:05:05 +0000

    datatypes

---
+++ .devcontainer/01_basic/02-datatypes.js
@@ -0,0 +1,17 @@
+"use strict"
+//alert(3+3)
+//primitive types
+//BigInt
+//Boolean
+//String
+//null
+//undefined
+//Symbol
+//number
+
+//non primitive
+//object
+ console.log(typeof"shivangi")
+ console.log(typeof null)
+ console.log(typeof true)
+ console.log(typeof isfollow)
