30012729517c36cdcee63b9598ab86021f3e1c83
shivangi prajapati <kumarisivangi754@gmail.com>
2026-02-04 04:57:14 +0000

    conversion

---
+++ .devcontainer/01_basic/03-conversion.js
@@ -0,0 +1,20 @@
+//let score="abc33"
+//console.log(typeof (33))
+//console.log(typeof ("33"))
+//conversion
+//let valueInnumber=Number(score)
+//console.log(typeof (valueInnumber))
+//console.log(valueInnumber)
+let isloggedin ="123"
+let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin)
+let somenumber="44ab"
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+let fewrollno=2345
+let Numberrollno=Number(fewrollno)
+console.log(Numberrollno)
+console.log(typeof Numberrollno)
+ 
