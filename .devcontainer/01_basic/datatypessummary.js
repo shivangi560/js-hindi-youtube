10dcc09c0792931424cc04ae6083d513f3cf7c39
shivangi prajapati <kumarisivangi754@gmail.com>
2026-02-05 17:06:58 +0000

    summary

---
+++ .devcontainer/01_basic/datatypessummary.js
@@ -0,0 +1,22 @@
+//primitive
+//7: string,number,boolean,null,Bigint,undefined,symbol
+let age
+const score=100
+const number=100.3
+const isloggedin=true
+const outidetemp=null
+const id=Symbol('1123')
+const anotherid=Symbol('1123')
+console.log(id===anotherid)
+console.log(typeof anotherid)
+
+//reference type(non primirtive type)
+// array , function , object
+const students={
+    name:"shivangi",
+    age:23,
+    religion:"hindu",
+}
+console.log(students)
+console.log(typeof students)
+// javascript is dynamic
