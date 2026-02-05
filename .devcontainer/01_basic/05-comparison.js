29e68eba922843321f816bb62b9b7c77362c7d09
shivangi prajapati <kumarisivangi754@gmail.com>
2026-02-05 16:26:48 +0000

    comparison

---
+++ .devcontainer/01_basic/05-comparison.js
@@ -0,0 +1,19 @@
+//console.log("2">1)
+//console.log(1="2")
+//console.log("2">=1)
+
+//typescript doesnot allow to use two different data types
+
+console.log(2 == 2)
+console.log(2 >= 2)
+console.log(3 > 2)
+
+console.log(null==1)
+console.log(null>=1)
+console.log(null!=1)  
+console.log(undefined==1)
+console.log(undefined!=1)
+console.log(undefined<=1) 
+
+console.log("2"==2)
+console.log("2"===2)
