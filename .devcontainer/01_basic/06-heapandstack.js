62979577e5ad95b5443d64a70af4af3529b7eb11
shivangi prajapati <kumarisivangi754@gmail.com>
2026-02-05 17:39:13 +0000

    heapandstack

---
+++ .devcontainer/01_basic/06-heapandstack.js
@@ -0,0 +1,18 @@
+let myyoutubename="shivangidotcom"
+let anotehrname=myyoutubename
+let anothername="shivadotcom"
+console.log(myyoutubename)
+console.log(anothername)
+
+let student=
+{
+    name: "shivangi",
+    email:"kuamridhd",
+}
+let student1=student
+console.log(student1)
+console.log(student)
+student1.email="hwiudhwiu"
+console.log(student["email"])
+console.log(student.email)
+console.log(student1.email)
