313ae1a3bd3663d007022ce16244af551a7be059
shivangi prajapati <kumarisivangi754@gmail.com>
2026-02-05 15:42:50 +0000

    04-conversion

---
+++ .devcontainer/01_basic/04-conversion.js
@@ -0,0 +1,32 @@
+let value=3
+let negvalue=-value
+//console.log(negvalue)
+//console.log(2+3)
+
+let number=100
+number++
+//console.log(number)
+ number--
+ //console.log(number)
+ ++number
+ //console.log(number)
+ --number
+ //console.log(number)
+
+ //let x=3
+ //let y=--x
+ //console.log(y)
+
+ let x = 3;
+const y = ++x;
+console.log(y)
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+console.log(y2)
+// x2 is 4n; y2 is 4n
+let x4 = 3;
+const y4 = --x4;
+console.log(y4)
+ 
